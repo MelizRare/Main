773fd5f47c8337209bdb32116a430fd0ddbbbd4e
MelizRare <MelizRare@yandex.ru>
2022-12-14 17:17:50 +0700

    solution added

--- program.cs
+++ program.cs
@@ -1,5 +1,13 @@
 string[] input_mass =new string[] {"hello", "2", "world", ":-)"};
-string[] rez_mass= new string[] {};
-
-for (int j= 0; j<input_mass.Length;j++)
-Console.Write(input_mass[j]+" ");
+string[] rez_mass= new string[] {" "};
+for (int i= 0; i<input_mass.Length;i++)
+    {
+        if((int)input_mass[i].Length<= 3)
+        {
+            rez_mass.Append(input_mass[i]).ToArray();
+        }
+    }
+for (int z= 0; z<rez_mass.Length;z++)
+    {
+        Console.Write("2 "+rez_mass[z]+" ");
+    }
